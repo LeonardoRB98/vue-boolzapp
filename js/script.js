3028c43f90f395b129234df4a87df695440644da
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-12-14 10:45:36 +0100

    milestone 1

--- js/script.js
+++ js/script.js
@@ -4,3 +4,36 @@
 // CSS diverse
 // Visualizzazione dinamica della lista contatti: tramite la direttiva v-for,
 // visualizzare nome e immagine di ogni contatto
+
+var app = new Vue({
+  el: '#main_container',
+  data: {
+    message: 'Hello Vue!',
+    contacts: [
+      {
+        name: 'Michele',
+		    avatar: 'img/avatar_2.png',
+		    visible: true,
+		    messages: [
+          {
+				    date: '20/03/2020 16:30:00',
+				    text: 'Ciao come stai?',
+				    status: 'sent'
+			},
+        ],
+      },
+      {
+        name: 'Fabio',
+		    avatar: 'img/avatar_3.png',
+		    visible: true,
+		    messages: [],
+      },
+      {
+        name: 'Samuele',
+		    avatar: 'img/avatar_4.png',
+		    visible: true,
+		    messages: [],
+      },
+    ]
+  }
+})
